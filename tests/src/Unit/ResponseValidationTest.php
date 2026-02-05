b70d57e4a05e7a2866964532070f4ea452895bea
Aryan-R-Patel <aryanp.031206@gmail.com>
2026-02-05 12:25:15 -0500

    feat: add test cases

--- tests/src/Unit/ResponseValidationTest.php
+++ tests/src/Unit/ResponseValidationTest.php
@@ -19,6 +19,7 @@ use Drupal\static_metadata_records\Plugin\MetadataExtractor\MODSExtractor;
  * @group static_metadata_records
  */
 class ResponseValidationTest extends UnitTestCase {
+	// VALID TESTS
     public function testDCResponseValidation() {
         $extractor = new DCExtractor();
         $body_to_parse = '<?xml version="1.0"?><OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/ http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd"><responseDate>2026-01-30T18:17:14Z</responseDate><request verb="GetRecord" metadataPrefix="oai_dc">https://islandora.dev/oai/request</request><GetRecord><record><header><identifier>oai:islandora.dev:node-101</identifier><datestamp>2026-01-30T15:20:17Z</datestamp><setSpec>oai_pmh_default_set:entity_reference_1</setSpec></header><metadata><oai_dc:dc xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd"><dc:title>Custom Module Testing Page</dc:title>
@@ -191,6 +192,96 @@ class ResponseValidationTest extends UnitTestCase {
         
         $this->assertEquals($expected, $actual);
     }
+
+	// EMPTY BODY 
+    public function testDCParseBodyEmpty(){
+        $extractor = new DCExtractor();
+        $body_to_parse = "";
+        $actual = $extractor->parseBody($body_to_parse);
+        $this->assertNull($actual);
+    }
+
+	public function testMODSParseBodyEmpty(){
+        $extractor = new MODSExtractor();
+        $body_to_parse = "";
+        $actual = $extractor->parseBody($body_to_parse);
+        $this->assertNull($actual);
+    }
+
+	// INVALID XML 
+	public function testDCInvalidXML(){
+		$extractor = new DCExtractor();
+		$body_to_parse = '<?xml version="1.0"?><OAI-PMH><incomplete>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertNull($actual);
+	}
+
+	public function testMODSInvalidXML(){
+		$extractor = new MODSExtractor();
+		$body_to_parse = '<?xml version="1.0"?><OAI-PMH><incomplete>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertNull($actual);
+	}
+
+	// NO XML TAG 
+	public function testDCNoXmlTag(){
+		$extractor = new DCExtractor();
+		$body_to_parse = '<OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"><responseDate>2026-01-30T18:17:14Z</responseDate><request verb="GetRecord" metadataPrefix="oai_dc">https://islandora.dev/oai/request</request><GetRecord><record><header><identifier>oai:islandora.dev:node-101</identifier></header><metadata><oai_dc:dc xmlns:dc="http://purl.org/dc/elements/1.1/"><dc:title>Test</dc:title></oai_dc:dc></metadata></record></GetRecord></OAI-PMH>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertNull($actual);
+	}
+
+	public function testMODSNoXmlTag(){
+		$extractor = new MODSExtractor();
+		$body_to_parse = '<OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"><responseDate>2026-01-30T18:23:56Z</responseDate><request verb="GetRecord" metadataPrefix="mods">https://islandora.dev/oai/request</request><GetRecord><record><header><identifier>oai:islandora.dev:node-101</identifier></header><metadata><mods xmlns="http://www.loc.gov/mods/v3"><titleInfo><title>Test</title></titleInfo></mods></metadata></record></GetRecord></OAI-PMH>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertNull($actual);
+	}
+
+	// NO OAI-PMG TAG
+	public function testDCNoOaiPmhTag(){
+		$extractor = new DCExtractor();
+		$body_to_parse = '<?xml version="1.0"?><some><xml><metadata><oai_dc:dc xmlns:dc="http://purl.org/dc/elements/1.1/"><dc:title>Test</dc:title></oai_dc:dc></metadata></xml></some>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertNull($actual);
+	}
+
+	public function testMODSNoOaiPmhTag(){
+		$extractor = new MODSExtractor();
+		$body_to_parse = '<?xml version="1.0"?><some><xml><metadata><mods xmlns="http://www.loc.gov/mods/v3"><titleInfo><title>Test</title></titleInfo></mods></metadata></xml></some>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertNull($actual);
+	}
+
+	// NO METADATA TAG
+	public function testDCNoMetadataTags(){
+		$extractor = new DCExtractor();
+		$body_to_parse = '<?xml version="1.0"?><OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"><responseDate>2026-01-30T18:17:14Z</responseDate><request verb="GetRecord" metadataPrefix="oai_dc">https://islandora.dev/oai/request</request><GetRecord><record><header><identifier>oai:islandora.dev:node-101</identifier></header></record></GetRecord></OAI-PMH>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertNull($actual);
+	}
+
+	public function testMODSNoMetadataTags(){
+		$extractor = new MODSExtractor();
+		$body_to_parse = '<?xml version="1.0"?><OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"><responseDate>2026-01-30T18:23:56Z</responseDate><request verb="GetRecord" metadataPrefix="mods">https://islandora.dev/oai/request</request><GetRecord><record><header><identifier>oai:islandora.dev:node-101</identifier></header></record></GetRecord></OAI-PMH>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertNull($actual);
+	}
+
+	// EMPTY METADATA TAGS
+	public function testDCEmptyMetadata(){
+		$extractor = new DCExtractor();
+		$body_to_parse = '<?xml version="1.0"?><OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"><responseDate>2026-01-30T18:17:14Z</responseDate><request verb="GetRecord" metadataPrefix="oai_dc">https://islandora.dev/oai/request</request><GetRecord><record><header><identifier>oai:islandora.dev:node-101</identifier></header><metadata></metadata></record></GetRecord></OAI-PMH>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertEquals('', $actual);
+	}
+
+	public function testMODSEmptyMetadata(){
+		$extractor = new MODSExtractor();
+		$body_to_parse = '<?xml version="1.0"?><OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"><responseDate>2026-01-30T18:23:56Z</responseDate><request verb="GetRecord" metadataPrefix="mods">https://islandora.dev/oai/request</request><GetRecord><record><header><identifier>oai:islandora.dev:node-101</identifier></header><metadata></metadata></record></GetRecord></OAI-PMH>';
+		$actual = $extractor->parseBody($body_to_parse);
+		$this->assertEquals('', $actual);
+	}
 }
 
 ?>
